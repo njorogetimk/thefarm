dd7d0c226e78a5db41da2da00b778bf6d473121a
Timothy Njoroge <njorogetimk@gmail.com>
2018-01-28 22:28:07 +0300

    clean up

--- the_App/static/js/custom.js
+++ the_App/static/js/custom.js
@@ -26,29 +26,3 @@ document.addEventListener('DOMContentLoaded', function() {
 let rfile = document.getElementById('real-file')
 let btn = document.getElementById('custom-button')
 let span = document.getElementById('custom-text')
-
-
-// // select the objects
-// let image = document.querySelector('.file-field')
-// image.firstElementChild.setAttribute('hidden', 'hidden')
-// img_inp = image.lastElementChild
-//
-// // Create a new div for materialize
-// let btnDiv = document.createElement('div')
-// btnDiv.className = 'btn'
-// let span = document.createElement('span')
-// span.innerHTML = 'File'
-// let input = document.createElement('input')
-// input.setAttribute('type', 'file')
-// btnDiv.appendChild(span)
-// btnDiv.appendChild(input)
-//
-// let filePathWrapper = document.createElement('div')
-// filePathWrapper.className = 'file-path-wrapper'
-// filePathWrapper.appendChild(img_inp)
-//
-//
-//
-// image.appendChild(btnDiv)
-// image.appendChild(filePathWrapper)
-// console.log(btnDiv);
