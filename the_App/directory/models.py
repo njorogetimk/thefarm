146e97c438aba43fdb8f9194128f9c1b8ebc7747
Timothy Njoroge <njorogetimk@gmail.com>
2019-04-05 16:02:37 +0300

    Add Password column

--- the_App/directory/models.py
+++ the_App/directory/models.py
@@ -4,6 +4,7 @@ from the_App import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(100))
     level = db.Column(db.Integer)
 
     def __init__(self, name, level):
