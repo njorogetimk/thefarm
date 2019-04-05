257b9dd1e50a32b429e4262f4bd5f9017f425b98
Timothy Njoroge <njorogetimk@gmail.com>
2019-04-05 21:46:18 +0300

    House keep

--- the_App/static/js/custom.js
+++ the_App/static/js/custom.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+// Form 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, {});
