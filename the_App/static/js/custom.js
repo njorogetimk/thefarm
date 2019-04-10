eaa613452c8c058bbc1cb6bc9416d129224ab5db
Timothy Njoroge <njorogetimk@gmail.com>
2019-04-10 12:45:10 +0300

    Add A collapsible menu list to display the animals

--- the_App/static/js/custom.js
+++ the_App/static/js/custom.js
@@ -9,3 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, {});
 });
+
+// Collapsible
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+  });
